7bedf60748cb0891dd3094d074b7f65e62afb25f
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-10 19:02:37 +0000

    Added new XCLASS
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/direct_mail_userfunc/trunk@25236 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/scripts/class.ux_tx_directmail_dmail.php
+++ res/scripts/class.ux_tx_directmail_dmail.php
@@ -31,13 +31,13 @@
  *
  * $Id$
  */
-class ux_tx_directmail_recipient_list extends tx_directmail_recipient_list {
+class ux_tx_directmail_dmail extends tx_directmail_dmail {
 
 	/**
-	 * Puts all recipients uid from all table into an array.
+	 * Gets the recipient IDs given only the group ID
 	 *
-	 * @param integer $group_uid: uid of the group
-	 * @return array list of the uid in an array
+	 * @param integer $group_uid: recipient group ID
+	 * @return array list of the recipient ID
 	 */
 	public function cmd_compileMailGroup($group_uid) {
 		$id_lists = array(
@@ -45,7 +45,7 @@ class ux_tx_directmail_recipient_list extends tx_directmail_recipient_list {
 			'fe_users'   => array(),
 			'PLAINLIST'  => array(),
 		);
-		if ($group_uid)	{
+		if ($group_uid) {
 			$mailGroup = t3lib_BEfunc::getRecord('sys_dmail_group', $group_uid);
 			if (is_array($mailGroup) && $mailGroup['pid'] == $this->id) {
 				switch ($mailGroup['type']) {
@@ -57,18 +57,21 @@ class ux_tx_directmail_recipient_list extends tx_directmail_recipient_list {
 								'PLAINLIST' => &$id_lists['PLAINLIST'],
 							);
 							t3lib_div::callUserFunction($itemsProcFunc, $params, $this);
+							$id_lists['PLAINLIST'] = tx_directmail_static::cleanPlainList($id_lists['PLAINLIST']);
 						}
 						break;
-					default:
-						return parent::cmd_compileMailGroup($group_uid);
+				default:
+					return parent::cmd_compileMailGroup($group_uid);		
 				}
 			}
 		}
 
+		// TODO: support hook from parent method?
+
 		$outputArray = array(
 			'queryInfo' => array('id_lists' => $id_lists)
 		);
-		return $outputArray;  
+		return $outputArray;
 	}
 
 }

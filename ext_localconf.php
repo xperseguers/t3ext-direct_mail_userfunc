3879f4c0279f771ea81c3299b9eb6907778f50bd
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-18 12:41:33 +0000

    Added extension configuration option to enable/disable sample user functions
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/direct_mail_userfunc/trunk@25520 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,11 +10,15 @@ $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/direct_mail/mod3/class.tx_directmail
 // Include wizard class
 include(t3lib_extMgm::extPath($_EXTKEY) . 'res/scripts/class.tx_directmailuserfunc_wizard.php');
 
-// Uncomment next line to get a sample user function to be used with
-// "user_testList->myRecipientList" as itemsProcFunc value
-//include(t3lib_extMgm::extPath($_EXTKEY) . 'samples/user_testlist.php');
+// Include sample user functions if needed
+if (TYPO3_MODE === 'BE') {
+	$extConf = unserialize($TYPO3_CONF_VARS['EXT']['extConf']['direct_mail_userfunc']);
+	if ($extConf['enableSamples']) {
+		include(t3lib_extMgm::extPath($_EXTKEY) . 'samples/user_testlist.php');
 
-// If you run TYPO3 4.3, ExtJS is available and you may uncomment next line instead
-// and will then have to use "user_testlist_extjs->myRecipientList" as itemsProcFunc value
-//include(t3lib_extMgm::extPath($_EXTKEY) . 'samples/user_testlist_extjs.php');
+		if (strcmp(substr(TYPO3_version, 0, 3), '4.3') >= 0) {
+			include(t3lib_extMgm::extPath($_EXTKEY) . 'samples/user_testlist_extjs.php');
+		}
+	}
+}
 ?>

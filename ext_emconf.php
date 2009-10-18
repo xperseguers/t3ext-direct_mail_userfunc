5491229a683db4ea7de9c02ea8578a99a1695b73
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-18 14:19:08 +0000

    Uploaded version 1.1.0 to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/direct_mail_userfunc/trunk@25528 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "direct_mail_userfunc"
 #
-# Auto generated 16-10-2009 20:05
+# Auto generated 18-10-2009 16:18
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.0',
+	'version' => '1.1.0',
 	'doNotLoadInFE' => 1,
 	'constraints' => array(
 		'depends' => array(
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"b7da";s:10:"README.txt";s:4:"1a6e";s:12:"ext_icon.gif";s:4:"a143";s:17:"ext_localconf.php";s:4:"b158";s:14:"ext_tables.php";s:4:"22ea";s:14:"ext_tables.sql";s:4:"f418";s:16:"locallang_db.xml";s:4:"5d6a";s:17:"locallang_tca.xml";s:4:"9e16";s:14:"doc/manual.sxw";s:4:"247f";s:50:"res/scripts/class.tx_directmailuserfunc_wizard.php";s:4:"2d60";s:44:"res/scripts/class.ux_tx_directmail_dmail.php";s:4:"5d9e";s:53:"res/scripts/class.ux_tx_directmail_recipient_list.php";s:4:"977b";s:21:"samples/parameters.js";s:4:"ba5b";s:25:"samples/user_testlist.php";s:4:"b857";s:31:"samples/user_testlist_extjs.php";s:4:"8e1c";}',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"0e9f";s:10:"README.txt";s:4:"1a6e";s:21:"ext_conf_template.txt";s:4:"4456";s:12:"ext_icon.gif";s:4:"a143";s:17:"ext_localconf.php";s:4:"c4cb";s:14:"ext_tables.php";s:4:"b997";s:14:"ext_tables.sql";s:4:"f418";s:13:"locallang.xml";s:4:"f868";s:16:"locallang_db.xml";s:4:"5d6a";s:17:"locallang_tca.xml";s:4:"59ce";s:49:"controller/class.tx_directmailuserfunc_wizard.php";s:4:"a6b6";s:14:"doc/manual.sxw";s:4:"1864";s:39:"xclass/class.ux_tx_directmail_dmail.php";s:4:"e5a3";s:48:"xclass/class.ux_tx_directmail_recipient_list.php";s:4:"60ed";s:21:"samples/parameters.js";s:4:"ba5b";s:25:"samples/user_testlist.php";s:4:"b857";s:31:"samples/user_testlist_extjs.php";s:4:"8e1c";}',
 	'suggests' => array(
 	),
 );

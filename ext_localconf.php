8db18ba9582901d142244dbc1d873a44b8bc8729
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-18 14:11:07 +0000

    Reorganized files
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/direct_mail_userfunc/trunk@25526 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,11 +4,11 @@ if (!defined('TYPO3_MODE')) {
 }
 
 // Register XCLASS to process recipient lists based on a user function
-$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/direct_mail/mod2/class.tx_directmail_dmail.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'res/scripts/class.ux_tx_directmail_dmail.php';
-$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/direct_mail/mod3/class.tx_directmail_recipient_list.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'res/scripts/class.ux_tx_directmail_recipient_list.php';
+$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/direct_mail/mod2/class.tx_directmail_dmail.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'xclass/class.ux_tx_directmail_dmail.php';
+$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/direct_mail/mod3/class.tx_directmail_recipient_list.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'xclass/class.ux_tx_directmail_recipient_list.php';
 
 // Include wizard class
-include(t3lib_extMgm::extPath($_EXTKEY) . 'res/scripts/class.tx_directmailuserfunc_wizard.php');
+include(t3lib_extMgm::extPath($_EXTKEY) . 'controller/class.tx_directmailuserfunc_wizard.php');
 
 // Include sample user functions if needed
 if (TYPO3_MODE === 'BE') {

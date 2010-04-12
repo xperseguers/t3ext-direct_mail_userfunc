0f8d4ca1acdc7bf7ab168b9897565d44389b0e5e
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-12 09:49:23 +0000

    Fixed bug #7212: Install Tool update wizard gives error (thanks to Stefan Neufeind)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/direct_mail_userfunc/trunk@32135 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,16 +8,16 @@ $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/direct_mail/mod2/class.tx_directmail
 $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/direct_mail/mod3/class.tx_directmail_recipient_list.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'xclass/class.ux_tx_directmail_recipient_list.php';
 
 // Include wizard class
-include(t3lib_extMgm::extPath($_EXTKEY) . 'controller/class.tx_directmailuserfunc_wizard.php');
+include_once(t3lib_extMgm::extPath($_EXTKEY) . 'controller/class.tx_directmailuserfunc_wizard.php');
 
 // Include sample user functions if needed
 if (TYPO3_MODE === 'BE') {
 	$extConf = unserialize($TYPO3_CONF_VARS['EXT']['extConf']['direct_mail_userfunc']);
 	if ($extConf['enableSamples']) {
-		include(t3lib_extMgm::extPath($_EXTKEY) . 'samples/user_testlist.php');
+		include_once(t3lib_extMgm::extPath($_EXTKEY) . 'samples/user_testlist.php');
 
 		if (strcmp(substr(TYPO3_version, 0, 3), '4.3') >= 0) {
-			include(t3lib_extMgm::extPath($_EXTKEY) . 'samples/user_testlist_extjs.php');
+			include_once(t3lib_extMgm::extPath($_EXTKEY) . 'samples/user_testlist_extjs.php');
 		}
 	}
 }

1d290b2149c11ad1f961807458b7b33a619c4799
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-24 17:45:34 +0000

    Updated checksums
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/direct_mail_userfunc/trunk@36014 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "direct_mail_userfunc"
+# Extension Manager/Repository config file for ext "direct_mail_userfunc".
 #
-# Auto generated 28-10-2009 11:26
+# Auto generated 24-07-2010 19:45
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"468c";s:10:"README.txt";s:4:"1a6e";s:21:"ext_conf_template.txt";s:4:"4456";s:12:"ext_icon.gif";s:4:"7050";s:17:"ext_localconf.php";s:4:"c4cb";s:14:"ext_tables.php";s:4:"f0ab";s:14:"ext_tables.sql";s:4:"4c70";s:13:"locallang.xml";s:4:"f868";s:16:"locallang_db.xml";s:4:"5d6a";s:17:"locallang_tca.xml";s:4:"59ce";s:49:"controller/class.tx_directmailuserfunc_wizard.php";s:4:"d8b2";s:14:"doc/manual.sxw";s:4:"7c4e";s:39:"xclass/class.ux_tx_directmail_dmail.php";s:4:"e5a3";s:48:"xclass/class.ux_tx_directmail_recipient_list.php";s:4:"60ed";s:21:"samples/parameters.js";s:4:"9475";s:25:"samples/user_testlist.php";s:4:"380a";s:31:"samples/user_testlist_extjs.php";s:4:"2084";}',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"e266";s:10:"README.txt";s:4:"1a6e";s:21:"ext_conf_template.txt";s:4:"4456";s:12:"ext_icon.gif";s:4:"7050";s:17:"ext_localconf.php";s:4:"41e5";s:14:"ext_tables.php";s:4:"f0ab";s:14:"ext_tables.sql";s:4:"4c70";s:13:"locallang.xml";s:4:"f868";s:16:"locallang_db.xml";s:4:"5d6a";s:17:"locallang_tca.xml";s:4:"59ce";s:49:"controller/class.tx_directmailuserfunc_wizard.php";s:4:"d8b2";s:14:"doc/manual.sxw";s:4:"c131";s:21:"samples/parameters.js";s:4:"9475";s:25:"samples/user_testlist.php";s:4:"380a";s:31:"samples/user_testlist_extjs.php";s:4:"2084";s:39:"xclass/class.ux_tx_directmail_dmail.php";s:4:"e5a3";s:48:"xclass/class.ux_tx_directmail_recipient_list.php";s:4:"60ed";}',
 	'suggests' => array(
 	),
 );

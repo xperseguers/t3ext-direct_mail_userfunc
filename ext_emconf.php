1542babca4a0d313bec5045fe7c1d740d67c37d4
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-02-16 15:14:23 +0000

    [BUGFIX] Fix compatibility with direct_mail 2.7.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/direct_mail_userfunc/trunk@58028 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Adds support for user functions to Direct Mail. Currently allows recipient lists to be defined using a user function.',
 	'category' => 'module',
 	'author' => 'Xavier Perseguers',
-	'author_email' => 'typo3@perseguers.ch',
+	'author_email' => 'xavier@causal.ch',
 	'shy' => '',
 	'dependencies' => 'direct_mail',
 	'conflicts' => '',
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'php' => '5.2.0-0.0.0',
 			'typo3' => '4.2.0-4.6.99',
-			'direct_mail' => '2.6.3-0.0.0',
+			'direct_mail' => '2.7.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
--- xclass/class.ux_tx_directmail_dmail.php
+++ xclass/class.ux_tx_directmail_dmail.php
@@ -1,29 +1,31 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2009 Xavier Perseguers (typo3@perseguers.ch)
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2012 Xavier Perseguers (xavier@causal.ch)
+ *  (c) 2009-2011 Xavier Perseguers (typo3@perseguers.ch)
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 
 /**
@@ -37,43 +39,65 @@ class ux_tx_directmail_dmail extends tx_directmail_dmail {
 	/**
 	 * Gets the recipient IDs given only the group ID
 	 *
-	 * @param integer $group_uid: recipient group ID
+	 * @param array $groups array of recipient group ID
 	 * @return array list of the recipient ID
 	 */
-	public function cmd_compileMailGroup($group_uid) {
+	public function cmd_compileMailGroup(array $groups) {
+		// If supplied with an empty array, quit instantly as there is nothing to do
+		if (!count($groups)) {
+			return;
+		}
+
+		// Looping through the selected array, in order to fetch recipient details
 		$id_lists = array(
 			'tt_address' => array(),
-			'fe_users'   => array(),
-			'PLAINLIST'  => array(),
+			'fe_users' => array(),
+			'PLAINLIST' => array(),
 		);
-		if ($group_uid) {
-			$mailGroup = t3lib_BEfunc::getRecord('sys_dmail_group', $group_uid);
+		foreach ($groups AS $group) {
+			// Testing to see if group ID is a valid integer, if not - skip to next group ID
+			if (t3lib_div::compat_version('4.6')) {
+				$group = t3lib_utility_Math::convertToPositiveInteger($group);
+			} else {
+				$group = t3lib_div::intval_positive($group);
+			}
+			if (!$group) {
+				continue;
+			}
+
+			$mailGroup = t3lib_BEfunc::getRecord('sys_dmail_group', $group);
 			if (is_array($mailGroup) && $mailGroup['pid'] == $this->id) {
 				switch ($mailGroup['type']) {
 					case 5:
+						$recipientList = array(
+							'tt_address' => array(),
+							'fe_users' => array(),
+							'PLAINLIST' => array(),
+						);
 						$itemsProcFunc = $mailGroup['tx_directmailuserfunc_itemsprocfunc'];
 						if ($itemsProcFunc) {
 							$params = array(
-								'groupUid'  => $group_uid,
-								'lists' => &$id_lists,
+								'groupUid' => $group,
+								'lists' => &$recipientList,
 								'userParams' => $mailGroup['tx_directmailuserfunc_params'],
 							);
 							t3lib_div::callUserFunction($itemsProcFunc, $params, $this);
 
-								// Make unique entries
-							$id_lists['tt_address'] = array_unique($id_lists['tt_address']);
-							$id_lists['fe_users'] = array_unique($id_lists['fe_users']);
-							$id_lists['PLAINLIST'] = tx_directmail_static::cleanPlainList($id_lists['PLAINLIST']);
+							// Make unique entries
+							$recipientList['tt_address'] = array_unique($recipientList['tt_address']);
+							$recipientList['fe_users'] = array_unique($recipientList['fe_users']);
+							$recipientList['PLAINLIST'] = tx_directmail_static::cleanPlainList($recipientList['PLAINLIST']);
 						}
 						break;
-				default:
-					return parent::cmd_compileMailGroup($group_uid);		
+					default:
+						return parent::cmd_compileMailGroup(array($group));
+						break;
 				}
+				$id_lists = array_merge_recursive($id_lists, $recipientList);
 			}
 		}
 
 		// TODO: support hook from parent method?
-
 		$outputArray = array(
 			'queryInfo' => array('id_lists' => $id_lists)
 		);

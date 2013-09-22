8e9b98726c203d7ef0cfc5856e0592ada1b07a36
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-09-22 10:01:38 +0000

    [BUGFIX] #52172: XCLASS does not work in TYPO3 6.x
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/direct_mail_userfunc/trunk@79947 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,8 +33,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'doNotLoadInFE' => 1,
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.2.0-0.0.0',
-			'typo3' => '4.2.0-4.7.99',
+			'php' => '5.3.7-5.4.99',
+			'typo3' => '4.5.0-6.1.99',
 			'direct_mail' => '2.7.0-0.0.0',
 		),
 		'conflicts' => array(
--- ext_localconf.php
+++ ext_localconf.php
@@ -7,6 +7,13 @@ if (!defined('TYPO3_MODE')) {
 $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/direct_mail/mod2/class.tx_directmail_dmail.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'xclass/class.ux_tx_directmail_dmail.php';
 $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/direct_mail/mod3/class.tx_directmail_recipient_list.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'xclass/class.ux_tx_directmail_recipient_list.php';
 
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['tx_directmail_dmail'] = array(
+	'className' => 'ux_tx_directmail_dmail',
+);
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['tx_directmail_recipient_list'] = array(
+	'className' => 'ux_tx_directmail_recipient_list',
+);
+
 // Include wizard class
 include_once(t3lib_extMgm::extPath($_EXTKEY) . 'controller/class.tx_directmailuserfunc_wizard.php');
 

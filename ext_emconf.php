44eacf8b80379b559fd53c06fd1d0f15615a785b
Xavier Perseguers <xavier@causal.ch>
2020-08-10 11:32:05 +0200

    [TASK] Kickstart compatibility with EXT:direct_mail v6.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,9 +26,9 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.1-dev',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-7.3.99',
+            'php' => '7.2.0-7.4.99',
             'typo3' => '8.7.0-9.5.99',
-            'direct_mail' => '5.2.0-5.2.99',
+            'direct_mail' => '5.2.0-6.0.99',
         ],
         'conflicts' => [],
         'suggests' => [],

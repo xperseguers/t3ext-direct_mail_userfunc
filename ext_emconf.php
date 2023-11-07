7abaa85e8ff0c462fc49a88896bee8432eb35ae8
Xavier Perseguers <xavier@causal.ch>
2023-11-07 10:32:21 +0100

    [TASK] Drop compatibility with TYPO3 < v10

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,8 +18,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.3.0-dev',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-8.2.99',
-            'typo3' => '8.7.0-11.5.99',
+            'php' => '7.4.0-8.2.99',
+            'typo3' => '10.4.0-11.5.99',
             'direct_mail' => '5.2.0-9.4.99',
         ],
         'conflicts' => [],
